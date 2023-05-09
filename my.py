dcd32887d2a32ee781efb5d512484b5acfdff27e
ahammedshamsad <132884535+ahammedshamsad@users.noreply.github.com>
2023-05-09 10:31:01 +0400

    Add files via upload

---
+++ my.py
@@ -0,0 +1,7 @@
+num1=25
+firstname=input ('whats your first name ?' )
+middlename=input ('last name ?')
+lastname=input('first name')
+fullname=firstname +' '+middlename+ ' '+lastname
+print('life is happy')
+
